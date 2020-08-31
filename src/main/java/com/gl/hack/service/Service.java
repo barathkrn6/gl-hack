e1a461bd75075f5b012ab306a88282a1d25e76ce
Barath Karunakaran <barath.karunakaran@cowrks.com>
2020-08-31 13:17:17 +0530

    basic project structure

---
+++ src/main/java/com/gl/hack/service/Service.java
@@ -0,0 +1,4 @@
+package com.gl.hack.service;
+
+public interface Service {
+}
---
+++ src/main/java/com/gl/hack/service/impl/ServiceImpl.java
@@ -0,0 +1,7 @@
+package com.gl.hack.service.impl;
+
+import com.gl.hack.service.Service;
+
+@org.springframework.stereotype.Service
+public class ServiceImpl implements Service {
+}
